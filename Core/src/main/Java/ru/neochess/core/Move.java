eea781388c48244c3b0bf52d72323bbbad460ed3
diviz <diviz.dev@gmail.com>
2017-01-04 02:17:31 +0300

    add simple move

--- Core/src/main/Java/ru/neochess/core/Move.java
+++ Core/src/main/Java/ru/neochess/core/Move.java
@@ -1,8 +1,36 @@
 package ru.neochess.core;
 
 /**
- * Created by znobischevdv on 10.12.2016.
+ * Содержит клетки хода, фигуру хода, а так же дополнительные флаги
+ * Created by diviz on 10.12.2016.
  */
 public class Move {
 
+    protected Cell from, to;
+    protected Figure figure;
+    protected boolean isCheck;
+
+
+    public Move(Cell from, Cell to, Figure figure, boolean isCheck) {
+        this.from = from;
+        this.to = to;
+        this.figure = figure;
+        this.isCheck = isCheck;
+    }
+
+    public Cell getFrom() {
+        return from;
+    }
+
+    public Cell getTo() {
+        return to;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public boolean isCheck() {
+        return isCheck;
+    }
 }
